269a29bb25bc5c67061b1a47a834307e5f01e36a
IbrahimBashir123 <ibrahimbashir11111@gmail.com>
2023-02-17 21:02:12 +0200

    Done step 8-a

---
+++ Exercises/php-basics-01/Step 8 - a: PHP Data types - Array/Step_a8.php
@@ -0,0 +1,4 @@
+<?php
+$grocery1 = ["Eggs", "Milk", "Cheese", "Water Pack", "Tissues", "Watermelon"];
+
+echo "Hello Sir, do you have ", $grocery1[0], ", ", $grocery1[1], ", and ", $grocery1[2], "? Also if you sell fruits can I find a ", $grocery1[sizeof($grocery1) - 1], "?\n";
