2adb86c7fa4a90c5f166b9514de44b474df225a6
IbrahimBashir123 <ibrahimbashir11111@gmail.com>
2023-02-17 22:10:52 +0200

    Done step 9

---
+++ Exercises/php-basics-01/Step 9 : PHP Debug - die() and var_dump()/Step_9.php
@@ -0,0 +1,7 @@
+<?php
+$name = "Ibrahim"; // Assigning the string "Ibrahim" to the variable $name
+var_dump($name); // Dumping the value of $name to the screen
+$last_name = "Bashir"; // Assigning the string "Bashir" to the variable $last_name
+var_dump($last_name); // Dumping the value of $last_name to the screen
+$age = 22; // Assigning the integer 22 to a new variable $age
+var_dump($age); // Dumping the value of $age to the screen
