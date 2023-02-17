b031b2e7ec3fd447c0a97f88079c97f47c9eb8ab
IbrahimBashir123 <ibrahimbashir11111@gmail.com>
2023-02-17 18:51:54 +0200

    Done step 3

---
+++ Exercises/php-basics-01/Step 3 : Variables lvl 1/Step_3.php
@@ -0,0 +1,10 @@
+<?php
+$name = 'Ibrahim Bashir';
+$email = 'ibrahimbashir11111@gmail.com';
+
+echo  "My name is ${name} \n and my email is ${email} .\n";
+
+$height  = 182;
+$width = 90;
+$area = $height*$width;
+echo $area;
