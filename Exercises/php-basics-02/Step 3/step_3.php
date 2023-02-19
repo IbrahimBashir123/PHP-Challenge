fa8a8b82811a92f6a94f5bf720f95343014741d1
IbrahimBashir123 <ibrahimbashir11111@gmail.com>
2023-02-19 19:03:50 +0200

    Done step 3

---
+++ Exercises/php-basics-02/Step 3/step_3.php
@@ -0,0 +1,15 @@
+<?php
+$var1= 32;
+$var2= 45;
+
+
+echo "Before swapping: var1= $var1, var2= $var2\n";
+
+// Swap the variables
+
+$temp = $var1;
+$var1 = $var2;
+$var2 = $temp;
+
+echo "After swapping: var1= $var1, var2= $var2";
+?>
